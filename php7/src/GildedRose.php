bf27159f605d64ea7cf8c92b5a3aea5111e3abdb
jimmy.huang <jimmy.huang@104.com.tw>
2020-08-20 16:56:32 +0800

    refactor: 將各個商品邏輯拆開

--- php7/src/GildedRose.php
+++ php7/src/GildedRose.php
@@ -16,50 +16,18 @@ final class GildedRose
     {
         $items = $this->getItems();
         foreach ($items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
-                    }
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sell_in < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                        if ($item->sell_in < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                $item->sell_in = $item->sell_in - 1;
-            }
-
-            if ($item->sell_in < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
-                            }
-                        }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
-                }
+            switch ($item->name) {
+                case 'Aged Brie':
+                    $this->updateAgedBrie($item);
+                    break;
+                case 'Sulfuras, Hand of Ragnaros':
+                    $this->updateSulfuras($item);
+                    break;
+                case 'Backstage passes to a TAFKAL80ETC concert':
+                    $this->updateBackstage($item);
+                    break;
+                default:
+                    $this->update($item);
             }
         }
     }
@@ -67,5 +35,62 @@ final class GildedRose
     public function getItems()
     {
         return $this->items;
+    }
+
+    private function updateAgedBrie($item)
+    {
+        if ($item->quality < 50) {
+            $item->quality = $item->quality + 1;
+        }
+
+        $item->sell_in = $item->sell_in - 1;
+
+        if ($item->sell_in < 0) {
+            if ($item->quality < 50) {
+                $item->quality = $item->quality + 1;
+            }
+        }
+    }
+
+    private function updateSulfuras($item)
+    {
+    }
+
+    private function updateBackstage($item)
+    {
+        if ($item->quality < 50) {
+            $item->quality = $item->quality + 1;
+            if ($item->sell_in < 11) {
+                if ($item->quality < 50) {
+                    $item->quality = $item->quality + 1;
+                }
+            }
+            if ($item->sell_in < 6) {
+                if ($item->quality < 50) {
+                    $item->quality = $item->quality + 1;
+                }
+            }
+        }
+
+        $item->sell_in = $item->sell_in - 1;
+
+        if ($item->sell_in < 0) {
+            $item->quality = $item->quality - $item->quality;
+        }
+    }
+
+    private function update($item)
+    {
+        if ($item->quality > 0) {
+            $item->quality = $item->quality - 1;
+        }
+
+        $item->sell_in = $item->sell_in - 1;
+
+        if ($item->sell_in < 0) {
+            if ($item->quality > 0) {
+                $item->quality = $item->quality - 1;
+            }
+        }
     }
 }
